96933c9db6923a405928f4f7d53d092ffd02f651
wxy <wangxiaoyun@iscas.cn.cn>
2024-04-10 16:08:00 +0800

    解决move_group_capabilities bug

--- xarm_moveit_config/launch/move_group.launch.py
+++ xarm_moveit_config/launch/move_group.launch.py
@@ -4,4 +4,5 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("xarm", package_name="xarm_moveit_config").to_moveit_configs()
+    moveit_config.move_group_capabilities = {"capabilities": "move_group/ExecuteTaskSolutionCapability"}
     return generate_move_group_launch(moveit_config)
